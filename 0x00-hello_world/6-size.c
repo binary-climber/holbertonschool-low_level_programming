53347fdf9a75b5cdc75d704b34ef182381bd00a3
binary-climber <jaimerodg@gmail.com>
2021-10-01 08:22:33 -0500

    The same for all

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  *
  */int main(void)
 {
-	printf("Size of a char: %d\n", sizeof(char));
-	printf("Size of an int: %d\n", sizeof(int));
-	prinft("Size of a long int: %d\n", sizeof(long int));
-	printf("Size of a long long int: %d\n", sizeof(long long int));
-	printf("Size of a float: %d\n", sizeof(float));
+	printf("Size of a char: %ld\n", sizeof(char));
+	printf("Size of an int: %ld\n", sizeof(int));
+	printf("Size of a long int: %ld\n", sizeof(long int));
+	printf("Size of a long long int: %ld\n", sizeof(long long int));
+	printf("Size of a float: %ld\n", sizeof(float));
 	return (0);
 }
