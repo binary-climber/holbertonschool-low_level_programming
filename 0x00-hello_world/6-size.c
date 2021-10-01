7193bf919fdc54dde2fdc147d4905b050c17cbda
binary-climber <jaimerodg@gmail.com>
2021-10-01 07:47:33 -0500

    The same for all

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  */int main(void)
 {
 	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
-	printf("Size of a int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
 	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
 	printf("Size of a long log int: %lu bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
