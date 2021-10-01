319010ddfc5188e74f1fa825554ddf5eb8ac6e4b
binary-climber <jaimerodg@gmail.com>
2021-10-01 07:56:37 -0500

    The same for all

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,10 +3,10 @@
  * size of variables
  */int main(void)
 {
-	printf("Siz of a char: %lu bytes(s)\n", sizeof(char));
-	printf("Siz of an int: %lu bytes(s)\n", sizeof(int));
-	printf("Siz of a long int: %lu bytes(s)\n", sizeof(long int));
-	printf("Siz of a long log int: %lu bytes(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
+	printf("Size of a char:%lu bytes(s)\n", sizeof(char));
+	printf("Size of an int:%lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int:%lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long log int:%lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a float:%lu bytes(s)\n", sizeof(float));
 	return (0);
 }
