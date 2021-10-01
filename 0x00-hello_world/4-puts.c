b76d1812e1c48b6da19eca5848b2ffe090a97549
binary-climber <jaimerodg@gmail.com>
2021-10-01 06:37:55 -0500

    The same for all

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char name[]="\"Programming is like building a multilingual puzzle";
+	char name[] = "\"Programming is like building a multilingual puzzle";
 	puts(name);
 	return (0);
 }
