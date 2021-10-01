174502a3a22f69f8c0605160d9877e1011dab5b4
binary-climber <jaimerodg@gmail.com>
2021-10-01 06:57:20 -0500

    The same for all

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
-/*
- * describing the programm
- */
 #include <stdio.h>
-
 /*
- * print but with with puts
- */
-int main(void)
+ *print but with with puts
+ */int main(void)
 {
 	puts("\"Programming is like building a multilingual pu\
 zzle");
