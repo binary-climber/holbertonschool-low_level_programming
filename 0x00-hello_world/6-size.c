e9bf6377629723dea261114d71f792fd14638508
binary-climber <jaimerodg@gmail.com>
2021-10-01 07:38:08 -0500

    The same for all

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * size of variables
+ */int main(void)
+{
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(float));
+	return (0);
+}
