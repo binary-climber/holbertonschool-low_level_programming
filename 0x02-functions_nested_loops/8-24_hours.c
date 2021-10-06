bbcce6b238fc19244e5fdc33784d790fbdd59cb3
binary-climber <jaimerodg@gmail.com>
2021-10-05 22:09:28 -0500

    The same for all

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * time stamp
+ */
+void jack_bauer(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i'\n');
+
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -30,4 +30,17 @@ int print_sign(int n);
 /* abs */
 int _abs(int);
 
+/* last digit */
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+
+
 #endif
