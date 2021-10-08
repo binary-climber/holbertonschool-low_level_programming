9a4fe7816ba6e6c42086ebf68821d3db3b1e7f9e
binary-climber <jaimerodg@gmail.com>
2021-10-08 08:49:49 -0500

    The same for all

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /*
- *abs
- */
-int _abs(int x)
+ * abs value
+ * Return: x or -x
+ */int _abs(int x)
 {
-
-
-	if ( x>=0 )
+	if (x >= 0)
 	{
-		return(x);
+		return (x);
 	}
 	else
 	{
-		return(-x);
+		return (-x);
 	}
 }
