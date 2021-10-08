8ee339d04752e0c1af7708787cdbef02b06dad94
binary-climber <jaimerodg@gmail.com>
2021-10-08 15:46:38 -0500

    The same for all

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,9 +5,33 @@
 void jack_bauer(void)
 {
 	int i;
+	int j;
+	int k;
+	int l;
 
-	for (i = '0'; i <= '9'; i++)
-		_putchar(i'\n');
+	for (l = '0'; l <= '2'; l++)
+	{
 
+	for (k = '0'; k <= '9'; k++)
+	{
+		if (l == '2' && k == '4' )
+		{
+			break;
+		}
+		
+			for (j = '0'; j <= '5'; j++)
+			{
+				for (i = '0'; i <= '9'; i++)
+					{
+						_putchar(l);
+						_putchar(k);
+						_putchar(':');
+						_putchar(j);
+						_putchar(i);
+						_putchar('\n');
+					}
+			}
+	}
+	}
 
 }
