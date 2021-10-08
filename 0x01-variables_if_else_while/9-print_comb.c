55b7d7b40b29c6d9075ad7b26036fec7fa31a5df
binary-climber <jaimerodg@gmail.com>
2021-10-08 08:12:51 -0500

    The same for all

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,22 +6,16 @@
 {
 	int i;
 
-	for (i = '0'; i <='9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
+		putchar(i);
 		if (i != '9')
 		{
-			putchar(i);
 			putchar(',');
 			putchar(' ');
 		}
-		else
-		{
-			putchar('9');
-			putchar(' ');
-		}
 	}
-	 return (0);
+	putchar('\n');
 
-
-
+	return (0);
 }
