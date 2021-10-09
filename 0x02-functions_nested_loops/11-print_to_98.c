e8a6a9ab10ad4af7244098b948a019fac8188dd2
binary-climber <jaimerodg@gmail.com>
2021-10-08 19:00:43 -0500

    The same for all

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,39 +6,35 @@
  *
  */void print_to_98(int n)
 {
-        int i;
+	int i;
 
-        if (n < 98)
-        {
-                for (i = n; i <= 98; i++)
-                {
-                        printf("%d", i);
-                        if (i != 98)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-
-                }
-        }
-
-        else if (n == 98)
-        {
-                printf("%d", n);
-        }
-
-        else
-        {
-                for (i = n; i >= 98; i--)
-                {
-                        printf("%d", i);
-                        if (i != 98)
-                        {
-                                putchar(',');
-                                putchar(' ');
-                        }
-
-                }
-        }
-        printf("\n");
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	else if (n == 98)
+	{
+		printf("%d", n);
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	printf("\n");
 }
