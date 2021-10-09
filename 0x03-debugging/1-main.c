d2ac1edc85e2d7fceebfb74a3cce5980826e8969
binary-climber <jaimerodg@gmail.com>
2021-10-08 21:53:24 -0500

    The same for all

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-	i= 0;
-
-/*	while (i < 10)
-	{
-		putchar(i + '0');
-		} */
-
+	i = 0;
+/*
+ *	while (i < 10)
+ *	{
+ *		putchar(i + '0');
+ *		}
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
