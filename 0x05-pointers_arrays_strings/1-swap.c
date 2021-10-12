6a5fc0df61985df6badf11ac5bd97c7c52ed3d01
binary-climber <jaimerodg@gmail.com>
2021-10-11 20:12:59 -0500

    The same for all

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps numbers
+ * @a: pointer of number 1
+ * @b: pointer of number 2
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	 b = #include "main.h"
+		 #include <stdio.h>
+
+		 /**
+		  * main - check the code
+		  *
+		  * Return: Always 0.
+		  */
+		 int main(void)
+		 {
+			 int a;
+			 int b;
+
+			 a = 98;
+			 b = 42;
+			 printf("a=%d, b=%d\n", a, b);
+			 swap_int(&a, &b);
+			 printf("a=%d, b=%d\n", a, b);
+			 return (0);
+		 }~b;
+	*b = a;
+}
